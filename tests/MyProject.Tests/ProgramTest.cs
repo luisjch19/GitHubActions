fbea19b732f56e8d6055bbc6802894d135add0dc
luisjch19 <101585701+luisjch19@users.noreply.github.com>
2022-08-10 06:07:06 -0600

    Update ProgramTest.cs

--- tests/MyProject.Tests/ProgramTest.cs
+++ tests/MyProject.Tests/ProgramTest.cs
@@ -1,6 +1,6 @@
 using System;
 using Xunit;
-using Validators.Password;
+
 
 namespace MyProject.Tests
 {
@@ -12,18 +12,6 @@ namespace MyProject.Tests
             Assert.True(true, "Should be true");
         }
         
-        [Fact]
-    public void ValidPassword()
-    {
-      //Arrange
-      var passwordValidator = new PasswordValidator();
-      const string password = "Th1sIsapassword!";
-
-      //Act
-      bool isValid = passwordValidator.IsValid(password);
-
-      //Assert
-      Assert.True(isValid, $"The password {password} is not valid");
-    }
+        
     }
 }
