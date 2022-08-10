b2483e1cb67893a04f88afaf2d04703dfb91cd61
luisjch19 <101585701+luisjch19@users.noreply.github.com>
2022-08-10 06:15:23 -0600

    Update MathOperation.cs

--- src/MyProject/MathOperation.cs
+++ src/MyProject/MathOperation.cs
@@ -1,1 +1,27 @@
+using System;
 
+namespace MyProject
+{
+    public class MathOperation  
+    {          
+        public static double Add(double number1, double number2)  
+        {             
+            return (number1 + number2);  
+        }  
+  
+        public static double Subtract(double number1, double number2)  
+        {  
+            return (number1 - number2);  
+        }  
+  
+        public static double Multiply(double number1, double number2)  
+        {  
+            return (number1 * number2);  
+        }  
+  
+        public static double Divide(double number1, double number2)  
+        {  
+            return (number1 / number2);  
+        }  
+    }  
+}
