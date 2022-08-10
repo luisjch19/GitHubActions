f60c40360033bca395caca2902c1d6bb728d595d
luisjch19 <101585701+luisjch19@users.noreply.github.com>
2022-08-10 06:08:05 -0600

    Update ProgramTest.cs

--- tests/MyProject.Tests/ProgramTest.cs
+++ tests/MyProject.Tests/ProgramTest.cs
@@ -12,6 +12,20 @@ namespace MyProject.Tests
             Assert.True(true, "Should be true");
         }
         
+        [Fact]
+  public void ValidEmail()
+  {
+      //Arrange
+    var mailManager = new MailManager();
+    const string mailAddress = "john.smith@company.com";
+ 
+      //Act
+      bool isValid = mailManager.IsValidAddress(mailAddress);
+ 
+      //Assert
+    Assert.True(isValid, $"The email {mailAddress} is not valid");
+  }
+        
         
     }
 }
